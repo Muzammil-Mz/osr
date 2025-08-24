986f359a334cd5015eb05ae14a24ad9b1298efe5
muzammil-mz <mujjuimran74@gmail.com>
2025-08-24 12:07:49 +0000

    changes for form

--- server/app.js
+++ server/app.js
@@ -1,25 +1,23 @@
-import express from "express"
+import express from "express";
 import path from "path"; // new line
 import { fileURLToPath } from "url"; // new line
-
 
 const __filename = fileURLToPath(import.meta.url); // new line
 const __dirname = path.dirname(__filename); // new line
 
-const app = express()
-const port = 5020
+const app = express();
+const port = 5020;
 
 app.use(express.static(path.join(__dirname, "build"))); // new line
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
-
 app.use((req, res) => {
-    res.status(404).json({ msg: "Invalid route" });
+  res.status(404).json({ msg: "Invalid route" });
 });
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+  console.log(`server running on port ${port}`);
+});
